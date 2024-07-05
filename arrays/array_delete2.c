3fedc52a7043b1d6e6338d398ece1eb961916419
KompellyManohar-16 <kompellymanohar16@gmail.com>
2024-07-05 15:21:40 +0530

    added folders

---
+++ arrays/array_delete2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,del,size;
+    printf("\n enter the size of the array : ");
+    scanf("%d",&size);
+    if(size>10)
+    {
+        printf("\n overflow not possible to enter more than 10 elements");
+    }
+    else
+    {
+            printf("\nenter the array elements : ");
+            for(i=0;i<size;i++)
+            {
+                scanf("%d",&a[i]);
+            }
+            printf("\nthe entered elements are : ");
+            for(i=0;i<size;i++)
+            {
+                printf("%d\t",a[i]);
+            }
+            del=a[size-1];
+            size--;
+            printf("\n the new array is:");
+            for(i=0;i<size;i++)
+            {
+                printf("%d\t",a[i]);
+            }
+            printf("\n the deleted element is : %d",del);
+    }
+    return 0;
+}
