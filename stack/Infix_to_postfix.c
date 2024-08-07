a33736f33a09ebe3e11e98b0f331808e95ac7df1
KompellyManohar-16 <kompellymanohar16@gmail.com>
2024-08-08 00:43:57 +0530

    stack infix to postfix

---
+++ stack/Infix_to_postfix.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void infix_to_postfix(char s[]);
+int precedence(char c);
+char associativity(char c); 
+
+void infix_to_postfix(char s[])
+{
+    char stack[1000];
+        int top=-1;
+
+    char postfix[1000];
+    int index=0;
+
+    int len=strlen(s);
+    printf("\n the length is : %d",len);
+
+
+    for(int i=0;i<len;i++)
+    {
+        char c=s[i];
+        if(c>='a'&&c<='z' || c>='A'&&c<='Z' || c>='0'&&c<='9')
+        {
+            postfix[index]=c;
+            index++;
+        }
+        else if(c=='(')
+        {
+            top++;
+            stack[top]=c;
+        }
+        else if(c==')')
+        {
+            while(top>=0 && stack[top]!='(')
+            {
+                postfix[index]=stack[top];
+                index++;
+                top--;
+            }
+            top--;
+        }
+        else
+        {
+            while(top>=0 && ((precedence(c) < precedence(stack[top])) || (precedence(c) == precedence(stack[top]) && associativity(c)=='L')))
+            {
+                postfix[index]=stack[top];
+                index++;
+                top--;
+            }
+            top++;
+            stack[top]=c;
+        }
+    }
+    while(top>=0)
+    {
+        postfix[index]=stack[top];
+        index++;
+        top--;
+    }
+    postfix[index]='\0';
+    printf("\n the postfix expression is : %s",postfix);
+}
+
+int precedence(char c)
+{
+    if(c=='^')
+        return 3;
+    else if(c=='*' || c=='/')
+        return 2;
+    else if(c=='+' || c=='-')
+        return 1;
+    else
+        return -1;
+}
+
+char associativity(char c)
+{
+    if(c=='^')
+        return 'R';
+    return 'L';
+}
+
+int main()
+{
+    char a[50]="a+b-c*d/e^f";
+    printf("\n the Infix expression is : %s",a);
+    infix_to_postfix(a);
+    return 0;
+}
+
