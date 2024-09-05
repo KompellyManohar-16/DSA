628cd45e4c4155cd0131a68a74cffca69487dec3
KompellyManohar-16 <kompellymanohar16@gmail.com>
2024-09-05 21:34:22 +0530

    traversing the matrix in snake pattern

---
+++ matrix/traversing_snake_pattern.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10][10],row,col;
+    printf("\n enter the no.of rows and columns : ");
+    scanf("%d %d",&row,&col);
+    printf("\n enter the values of the matrix : ");
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\n the matrix is : \n");
+    for(int j=0;j<row;j++)
+    {
+        for(int k=0;k<col;k++)
+        {
+            printf("%d\t",arr[j][k]);
+        }
+        printf("\n");
+    }
+    printf("\n the elements in snake pattern : \n");
+    for(int i=0;i<row;i++)
+    {
+        if(i%2==0)
+        {
+                for(int j=0;j<col;j++)
+                {
+                    printf("%d\t",arr[i][j]);
+                }
+        }
+        else
+        {
+                for(int j=col-1;j>=0;j--)
+                {
+                    printf("%d\t",arr[i][j]);
+                }
+        }
+        printf("\n");
+    }
+    return 0;
+}
