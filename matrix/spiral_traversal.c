09dc723866370557ffc087c5a3c7d16a121367ae
KompellyManohar-16 <kompellymanohar16@gmail.com>
2024-09-07 21:22:44 +0530

    traversing the matrix spirally 	new file:   matrix/spiral_traversal.c

---
+++ matrix/spiral_traversal.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10][10],row,col;
+    printf("\n enter the no.of rows and columns : ");
+    scanf("%d %d",&row,&col);
+    printf("\n enter the values of matrix : ");
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("\n the entered matrix : \n");
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n spirally traversing the matrix : \n");
+    int top=0,bottom=row-1;
+    int left=0,right=col-1;
+    while(top<=bottom && left<=right)
+    {
+        for(int i=top;i<=right;i++)
+        {
+            printf("%d\t",arr[top][i]);
+        }
+        top++;
+        if(top<=bottom && left<=right)
+        {
+            for(int i=top;i<=bottom;i++)
+            {
+                printf("%d\t",arr[i][right]);
+            }
+            right--;
+        }
+        if(left<=right && top<=bottom)
+        {
+            for(int i=right;i>=left;i--)
+            {
+                printf("%d\t",arr[bottom][i]);
+            }
+            bottom--;
+        }
+        if(top<=bottom && left<=right)
+        {
+            for(int i=bottom;i>=top;i--)
+            {
+                printf("%d\t",arr[i][left]);
+            }
+            left++;
+        }
+    }
+    return 0;
+}
