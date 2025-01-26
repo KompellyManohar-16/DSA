a41a1d3a5197c93bf9eaf16f8f0f5057b395339e
KompellyManohar-16 <kompellymanohar16@gmail.com>
2025-01-27 00:35:23 +0530

    cycle detection in undirected graph by bfs and dfs

--- graphs/cycle_detection_in_undirected.c
+++ graphs/cycle_detection_in_undirected.c
@@ -55,6 +55,59 @@ int cycle_detection_bfs(int edges[][2], int no_of_edges, int no_of_vertices)
     return 0;
 }
 
+int cycle_detection_dfs(int edges[][2], int no_of_edges, int no_of_vertices)
+{
+    int adj_matrix[MAX][MAX]={0};
+    for(int i=0;i<no_of_edges;i++)
+    {
+        int a=edges[i][0];
+        int b=edges[i][1];
+
+        adj_matrix[a][b]=1;
+        adj_matrix[b][a]=1;
+    }
+
+    int stack[MAX],top=-1;
+    int visited[no_of_vertices];
+    int parent[no_of_vertices];
+
+    for(int i=0;i<no_of_vertices;i++)
+    {
+        visited[i]=0;
+        parent[i]=-1;
+    }
+
+    for(int start=0; start < no_of_vertices; start++)
+    {
+        if( !visited[start] )
+        {
+            top=-1;
+
+            stack[++top]=start;
+
+            while(top>=0)
+            {
+                int current=stack[top--];
+                visited[current]=1;
+
+                for(int i=0;i<no_of_vertices;i++)
+                {
+                    if( adj_matrix[current][i] == 1 && !visited[i] )
+                    {
+                        stack[++top]=i;
+                        parent[i]=current;
+                    }
+                    else if( adj_matrix[current][i] == 1 && visited[i]==1 && parent[current] != i)
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
 int main()
 {
     int no_of_vertices,no_of_edges;
@@ -64,14 +117,14 @@ int main()
     if(no_of_vertices <= 0 || no_of_vertices > MAX)
     {
         printf("\n Invalid input.. exiting..!");
-        return;
+        return 0;
     }
     printf("\n enter the no of edges : ");
     scanf("%d",&no_of_edges);
     if(no_of_edges <=0)
     {
         printf("\n Invalid input.. exiting..!");
-        return;
+        return 0;
     }
 
     int edges[no_of_edges][2];
@@ -99,5 +152,15 @@ int main()
     {
         printf("\n The graph doesn't contains cycle");
     }
+
+    if( cycle_detection_dfs(edges,no_of_edges,no_of_vertices))
+    {
+        printf("\n The graph contains cylce");
+    }
+    else
+    {
+        printf("\n The graph doesn't contains cycle");
+    }
+
     return 0;
 }
